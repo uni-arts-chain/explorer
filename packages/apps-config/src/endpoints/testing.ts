751a033c450b0d637a0f65a106b60296b7225dac
bari <br45604560>
2021-08-23 16:12:29 +0800

    RemoveIsUnreachable

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -407,7 +407,7 @@ export function createTesting (t: TFunction, firstOnly?: boolean): LinkOption[] 
     },
     {
       info: 'uniarts',
-      isUnreachable: true,
+      // isUnreachable: true,
       text: t('rpc.test.uniarts', 'UniArts', { ns: 'apps-config' }),
       providers: {
         UniArts: 'wss://testnet.uniarts.network'
